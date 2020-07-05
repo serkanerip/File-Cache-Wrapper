27ebb148a3ee235e689e71372698fafd7762ac2b
Serkan Erip <serkanerip@gmail.com>
2020-07-05 13:36:56 +0300

    First Commit

---
+++ main.php
@@ -0,0 +1,49 @@
+<?php
+
+class FCClosure
+{
+    public static function getCachedFunc(string $fileName, string $expTime, callable $func, bool $disable=false)
+    {
+        if (empty(str_replace(" ", "", $fileName))) {
+            throw new Exception("Filename cannot bet empty!");
+        }
+        $trimmedFileName = trim($fileName);
+        $filePath = $trimmedFileName[0] == "/" ? $trimmedFileName : "/tmp/$trimmedFileName";
+
+        if (strtotime($expTime) === false) {
+            throw new Exception("Exptime should be a valid strtotime string.");
+        }
+
+        if ($disable) {
+            return $func;
+        }
+
+        return function() use($filePath, $func, $expTime)
+        {
+            if (file_exists($filePath) && filesize($filePath) > 42)
+            {
+                $fileExpTime = file_get_contents($filePath, false, null,0,10);
+                if ($fileExpTime > strtotime("now"))
+                {
+                    $fileHashAndData = file_get_contents($filePath, false, null, 10);
+                    $fileHash = substr($fileHashAndData, 0, 32);
+                    $fileData = substr($fileHashAndData, 32, strlen($fileHashAndData));
+
+                    if ($fileHash === md5($fileData)) {
+                        return unserialize($fileData);
+                    } else {
+                        error_log("MD5 Fail");
+                    }
+                }
+            }
+
+            $data = ($func)();
+            $encodedData = serialize($data);
+            $fileContent = strtotime($expTime).md5($encodedData).$encodedData;
+            if (file_put_contents($filePath, $fileContent) === false) {
+                throw new Exception("Cannot write to $filePath");
+            }
+            return $data;
+        };
+    }
+}
