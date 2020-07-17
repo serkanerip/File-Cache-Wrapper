feaeb8debb8d8bfaa499e0606845ce39e2f5c06b
Serkan Erip <serkanerip@gmail.com>
2020-07-17 11:30:17 +0300

    locker added

--- src/main.php
+++ src/main.php
@@ -22,11 +22,16 @@ class FileCacheWrapper
 
         return function() use($filePath, $func, $expTime)
         {
+            $fileResource = fopen($filePath, "w+");
+
             if (file_exists($filePath) && filesize($filePath) > 42)
             {
+                flock($fileResource, LOCK_EX);
                 $fileExpTime = file_get_contents($filePath, false, null,0,10);
                 if ($fileExpTime > strtotime("now"))
                 {
+                    // if there is no need to write unlock the file.
+                    flock($fileResource, LOCK_UN);
                     $fileHashAndData = file_get_contents($filePath, false, null, 10);
                     $fileHash = substr($fileHashAndData, 0, 32);
                     $fileData = substr($fileHashAndData, 32, strlen($fileHashAndData));
@@ -37,6 +42,10 @@ class FileCacheWrapper
                         error_log("MD5 Fail");
                     }
                 }
+            } else
+            {
+                // Lock the file for writing cache for the first time.
+                flock($fileResource, LOCK_EX);
             }
 
             $data = ($func)();
@@ -45,7 +54,13 @@ class FileCacheWrapper
             if (file_put_contents($filePath, $fileContent) === false) {
                 throw new Exception("Cannot write to $filePath");
             }
+            // UNLOCK File after writing operations.
+            flock($fileResource, LOCK_UN);
             return $data;
         };
     }
 }
+
+$f = FileCacheWrapper::getCachedFunc("t.txt", "now", function(){return date("H:i:s  ");});
+
+echo $f();
